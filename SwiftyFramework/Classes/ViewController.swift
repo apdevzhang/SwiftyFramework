36735caa22cae8bc5d862188ec4738f43c6894fd
BANYAN <jesusblessher@gmail.com>
2020-03-17 17:06:06 +0800

    Classes

---
+++ SwiftyFramework/Classes/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SwiftyFramework
+//
+//  Created by BANYAN on 2020/3/17.
+//  Copyright © 2020 BANYAN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        DDLogInfo("Skoal~")
+    }
+
+
+}
+
